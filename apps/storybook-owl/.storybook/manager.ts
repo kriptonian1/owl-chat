803a5af1caf99537e865da2d57a1c377eb420618
kriptonian <shymal.sawan@gmail.com>
2023-08-11 13:36:00 +0530

    doc: storybook intro

---
+++ apps/storybook-owl/.storybook/manager.ts
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/manager-api';
+import owlStoryTheme from './owlStoryTheme';
+
+addons.setConfig({
+	theme: owlStoryTheme
+})
---
+++ apps/storybook-owl/.storybook/owlStoryTheme.ts
@@ -0,0 +1,13 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+    base: "dark",
+    brandTitle: "Owl Chat",
+	brandUrl: "",
+	brandImage: "https://i.postimg.cc/sX7qt8rS/logo-full-1.png",
+	brandTarget: "_self",
+
+	// UI
+	// appBg: "#2B2B2B",
+	// appContentBg: "#2B2B2B",
+});
